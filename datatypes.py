3463277834200e4c08c3b87aa2ab8e20c29c357a
iancoop999 <icooperjr949@boyslatin.net>
2026-09-04 12:58:44 +0000

    first commit

---
+++ datatypes.py
@@ -0,0 +1,35 @@
+#Complexity - The interconnected working parts of a system that are generally difficult to understand and maintain.
+#Abstraction - the practice of hiding complex systems to make it simpler and easier to manage.
+#GPU - Graphical Proccessing Unit. The brain of the computer, handles all the complex systems and processes that the computer performs
+#CPU - Central Proccessing Unit - The brain of the computer that handles all of the complex systems and processes that the computer performs.\
+# A string is a block of characters wrapped in quotes. Characters can be
+# Numbers, Letters or Symbols
+# you can use both single and double quotations
+# marks for strings.
+# FLOATS- A number with a decimal.
+#Booleans: It can only be entered by data types.
+#the hastag is a marker that are just whole numbers, or numbers 
+# without a decimal point.
+# we don't have to add any special symbols. we can just write the number
+# as it is.
+
+
+# "The apple count" Float
+# "Algebros" String
+# "Traveling, Off"  Boolean
+# "Bronze, Low class" "Silver, Middle Class." "Gold, Ultra Class"
+# "Mountain Dew", $3.99
+# "Dr Pepper", $3.99
+# "Coca Cola",  $4.99
+
+# Variable Naming Conventions:
+# Camel Case - Every new word begins with a capital letter.
+# the reason why its called camel case is bc, every new word
+# represents a hump (CAMEL CASE)
+# Snake Case - when creating a variable using snake case,
+# we use an underscore symbol to space out each new word
+# the reason why its snake case is bc the underscore 
+# represents the snake on the ground
+# Pascal Case - Pascal case is the same as camel case EXCEPT,
+# all words are starting with a capital letter
+
---
+++ variableActivy.py
@@ -0,0 +1,10 @@
+# variable - a named container that can store data inside, and you can
+# name your container almost anything you want. so long
+# as it is descriptive
+
+name = "Ian Cooper"
+
+# when we want to create a variable, we make up a word
+# use the equal sign and then, sign it a data type.
+#wheever the computer sees out variable word, it'll
+# use the data inside.
