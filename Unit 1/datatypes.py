15fa82452412637c227d334cb9208ca248579a0b
iancoop999 <icooperjr949@boyslatin.net>
2026-09-09 12:54:36 +0000

    warm up

--- Unit 1/datatypes.py
+++ Unit 1/datatypes.py
@@ -32,4 +32,3 @@
 # represents the snake on the ground
 # Pascal Case - Pascal case is the same as camel case EXCEPT,
 # all words are starting with a capital letter
-
---
+++ Unit 1/operators.py
@@ -0,0 +1,23 @@
+# Assignment operatos are exclusively used to assign values to variables.
+# key / used pairings
+
+# We use a single equal sign to represent the assignment operator.
+# Arithmetic operators - used on numerical data types to perform calculations.
+# intergers (whole numbers) and floats (decimal numbers)
+3 + 3 # addition operator
+3 - 3 # subtraction operator
+12 / 3 # division operator
+3 * 4 # multiplication order
+
+# Print is a function that lets us show code in the terminal
+# Comparison operators are at set of symbols used to assess if data is the same of different, and how they differ.
+(10 > 1) # greater than operator
+(10 < 2) # less than operator
+
+
+
+
+
+name = ian cooper
+grade = 10
+school = true
